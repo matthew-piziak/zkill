852899f2af3d161c70749e145e49a0bb0bb93c75
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-31 15:01:13 -0400

    parse out losses

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Rust wrapper for the zKillboard API.
 
+use std::collections::BTreeMap;
+
 extern crate hyper;
 use hyper::Client;
 
@@ -10,18 +12,46 @@ use rustc_serialize::json::Json;
 // filter out item flag 87 for drones
 // filter out ship ID 670 for pods
 
+#[derive(Debug)]
+struct Loss {
+    kill_id: u64,
+    victim_ship_type_id: u64,
+}
+
 pub fn sound_losses() {
     use std::io::prelude::*;
 
     let client = Client::new();
     let endpoint = endpoint(false, true, 99000739);
-    let mut response = client.get(&endpoint).send().unwrap();
+    let mut response = client.get(&endpoint).send().expect("Could not read API");
 
     let mut response_string = String::new();
-    response.read_to_string(&mut response_string).unwrap();
-    let data = Json::from_str(&response_string).unwrap();
-    let kills = data.as_array().unwrap();
-    println!("First kill: {}", kills.first().unwrap());
+    response.read_to_string(&mut response_string).expect("Could not read response");
+    let data = Json::from_str(&response_string).expect("Could not parse into JSON");
+    let losses = data.as_array().expect("Could not read as array");
+    for loss_json in losses {
+        let loss = loss(loss_json.as_object().expect("Could not read as object"));
+        println!("{:?}", loss);
+    }
+}
+
+fn loss(loss: &BTreeMap<String, Json>) -> Loss {
+    let kill_id = loss.get("killID")
+                      .expect("Could not read kill_id")
+                      .as_u64()
+                      .expect("kill_id not u64");
+    let victim = loss.get("victim")
+                     .expect("Could not read victim")
+                     .as_object()
+                     .expect("victim not object");
+    let victim_ship_type_id = victim.get("shipTypeID")
+                                    .expect("Could not read ship_id")
+                                    .as_u64()
+                                    .expect("ship_id not u64");
+    Loss {
+        kill_id: kill_id,
+        victim_ship_type_id: victim_ship_type_id,
+    }
 }
 
 fn endpoint(include_kills: bool, include_losses: bool, alliance_id: usize) -> String {
