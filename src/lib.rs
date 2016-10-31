e36eed0fa8f4fc09cf846173d29b1542268e7753
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-31 10:54:09 -0400

    parse sound losses to raw JSON object

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,53 @@
 
 extern crate hyper;
 use hyper::Client;
-use hyper::client::response::Response;
+
+extern crate rustc_serialize;
+use rustc_serialize::json::Json;
+
+// filter out item flag 5 for cargo
+// filter out item flag 87 for drones
+// filter out ship ID 670 for pods
+
+pub fn sound_losses() {
+    use std::io::prelude::*;
+
+    let client = Client::new();
+    let endpoint = endpoint(false, true, 99000739);
+    let mut response = client.get(&endpoint).send().unwrap();
+
+    let mut response_string = String::new();
+    response.read_to_string(&mut response_string).unwrap();
+    let data = Json::from_str(&response_string).unwrap();
+    let kills = data.as_array().unwrap();
+    println!("First kill: {}", kills.first().unwrap());
+}
+
+fn endpoint(include_kills: bool, include_losses: bool, alliance_id: usize) -> String {
+    if !include_kills && !include_losses {
+        panic!("No results will be returned.")
+    }
+    format!("https://zkillboard.com/api/allianceID/{}{}{}",
+            alliance_id,
+            if include_kills {
+                "/kills"
+            } else {
+                ""
+            },
+            if include_losses {
+                "/losses"
+            } else {
+                ""
+            })
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_basic_request() {
-        extern crate hyper;
-        use hyper::Client;
-
-        let client = Client::new();
-        let response = client.get("https://zkillboard.com/api/kills/characterID/268946627/")
-                             .send()
-                             .unwrap();
+    fn test_sound_losses() {
+        sound_losses();
+        assert_eq!(false, true);
     }
 }
